2b2e75b571ffeb5ab7d2798001237085cc4db2e6
Jack Verdin <jackverdin@icloud.com>
2026-07-09 18:11:44 -0400

    PokeSaveStats.py: Created very basic logic for inputting and saving pokemon stats (proof-of-concept), created saved_pokemon.txt

--- PokeSaveStats.py
+++ PokeSaveStats.py
@@ -6,12 +6,30 @@
 """
 
 from pathlib import Path
-import json
+
+savefile = Path("saved_pokemon.txt")
+pokelist = savefile.read_text()
+# Name HP Attack Defence SpAttack SpDefence Type1 Type2
+print(pokelist)
+
+
+given_name = input("Name: ")
+given_hp = input("HP: ")
+given_atk = input("Attack: ")
+given_def = input("Defence: ")
+given_satk = input("Special Attack: ")
+given_sdef = input("Special Defence: ")
+given_type1 = input("Type1: ")
+given_type2 = input("Type2: ")
+
+pokelist = f"{pokelist}\n{given_name} {given_hp} {given_atk} {given_def} {given_satk} {given_sdef} {given_type1} {given_type2}"
+
+savefile.write_text(pokelist)
 
 # BASIC IDEA: Subject To Change
 # Ask user what to do: Simulate or Add New Pokemon
     # Adding a new Pokemon, get input for all the stats and a name
-    # Save the details to a json file
+    # Save the details to a text file
     # Loop back to start
 # Simulate battle
 # Program extracts stats and names saved in file
