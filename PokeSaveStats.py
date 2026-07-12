50c6769bf778e8367c3d4ce9ff921037cdc20d0f
Jack Verdin <jackverdin@icloud.com>
2026-07-12 17:19:31 -0400

    PokeSaveStats.py: Changed addnewentry to rely on attributes rather than input(), making it more flexible. PokeSaveStats.py no longer imports from PokeDamageCalc.py to prevent circular import errors in the future.

--- PokeSaveStats.py
+++ PokeSaveStats.py
@@ -7,22 +7,13 @@
 
 from pathlib import Path
 import typechart
-from PokeDamageCalc import valid_input
 
 savefile = Path("saved_pokemon.txt")
 pokelist = savefile.read_text().rstrip()
 
-def addnewentry():
-    # Name HP Attack Defence SpAttack SpDefence Type1 Type2
-    given_name = input("Name: ")
-    given_hp = input("HP: ")
-    given_atk = input("Attack: ")
-    given_def = input("Defence: ")
-    given_satk = input("Special Attack: ")
-    given_sdef = input("Special Defence: ")
-    given_type1 = valid_input("Type1: ", typechart.type_list)
-    given_type2 = valid_input("Type2: ", typechart.type_list)
-    pokelist = f"{pokelist}\n{given_name} {given_hp} {given_atk} {given_def} {given_satk} {given_sdef} {given_type1} {given_type2}"
+def addnewentry(name, hp, patk , pdef, satk, sdef, type1, type2):
+    # Name HP PhAttack PhDefence SpAttack SpDefence Type1 Type2
+    pokelist = f"{pokelist}\n{name} {hp} {patk} {pdef} {satk} {sdef} {type1} {type2}"
     savefile.write_text(pokelist)
 
 """ TESTING CODE
