53037e62b1d84c39b5eb64b8a7e550b6b6b89532
Jack Verdin <jackverdin@icloud.com>
2026-07-13 14:05:06 -0400

    PokeSaveStats.py: Created readpokelist() function to create a way for the program to easily read from saved_pokemon.txt

--- PokeSaveStats.py
+++ PokeSaveStats.py
@@ -12,25 +12,50 @@ savefile = Path("saved_pokemon.txt")
 pokelist = savefile.read_text().rstrip()
 
 def addnewentry(name, hp, patk , pdef, satk, sdef, type1, type2):
+    savefile = Path("saved_pokemon.txt")
+    pokelist = savefile.read_text().rstrip()
     # Name HP PhAttack PhDefence SpAttack SpDefence Type1 Type2
     pokelist = f"{pokelist}\n{name} {hp} {patk} {pdef} {satk} {sdef} {type1} {type2}"
     savefile.write_text(pokelist)
 
-""" TESTING CODE
+def readpokelist(getvalues = False):
+    """Reads from saved_pokemon.txt and outputs the stored information as a dict. Can optionally provide a key to get the values of the dict directly.
+
+    Args:
+        getvalues (str, optional): If provided acts as a key for the dict. Defaults to False.
+
+    Returns:
+        list/dict/bool: Returns the entire dict if getvalues is False, returns a list of values if getvalues is a valid key, returns False if a KeyError was triggered
+    """
+    savefile = Path("saved_pokemon.txt")
+    pokelist = savefile.read_text().rstrip()
     biglist = []
     listnum = 0
+
+    # Separate each line into their own strings, and split each line into a list
     for line in pokelist.splitlines():
         biglist.append([])
         for bit in line.split():
             biglist[listnum].append(bit)
         listnum +=1
 
-    print(biglist)
-
-    namesofpoke = []
+    # Creates a dict with the name of the pokemon being the key and the value being a list containing all the stats and details
+    outputdict = {}
     for chunks in biglist:
-        namesofpoke.append(chunks[0])
-"""
+        outputdict[chunks[0]] = [int(chunks[1]), int(chunks[2]), int(chunks[3]), int(chunks[4]), int(chunks[5]), chunks[6], chunks[7]]
+
+    # If getvalues is the default (False), then the entire dict is outputted
+    # If a value is given and it triggers a KeyError, False is returned
+    # If a value is given and it matches a key, it returns the corresponding value
+    if getvalues:
+        try:
+            outputvalues = outputdict[getvalues]
+        except KeyError:
+            return False
+        else:
+            return outputvalues
+    else:
+        return outputdict
 
 # BASIC IDEA: Subject To Change
 # Ask user what to do: Simulate or Add New Pokemon
